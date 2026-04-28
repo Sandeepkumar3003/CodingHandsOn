54771d082048a17ce80395b5751e21c15d5d521c
Sandeepkumar <sandeepkumarrk1112@gmail.com>
2026-04-28 09:51:11 +0530

    Array Problems

---
+++ src/com/p7_BS_Easy/A1_SearchXinSortedArray.java
@@ -0,0 +1,34 @@
+package com.p7_BS_Easy;
+
+public class A1_SearchXinSortedArray {
+    public static void main(String[] args) {
+        int[] a = {3, 4, 6, 7, 9, 12, 16, 17}; // sorted array
+        int target = 11; // target element to search
+
+        int ind = binarySearchSolution(a, target);
+        if(ind==-1){
+            System.out.println("Target not found");
+        }else {
+            System.out.println(ind);
+        }
+    }
+
+    private static int binarySearchSolution(int[] a, int target) {
+        int left = 0;
+        int right = a.length-1;
+        int mid;
+        int index = -1;
+        while (left<=right){
+            mid = left+(right-left)/2;
+            if(target==a[mid]){
+                index = mid;
+                return index;
+            }else if(target<a[mid]){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return index;
+    }
+}
